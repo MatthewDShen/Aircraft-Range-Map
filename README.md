a8bf158141462aee99517aa61b1b4cd0e303342a
Matthew Shen <matthewdshen@gmail.com>
2022-03-28 16:23:48 -0400

    made geojson to json

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # URPL-GP-4560-Final-Project
- 
+
+The following project is a map generated as the Final Project for URPL-GP 4650. More information on the class can be found here: https://wagner.nyu.edu/education/courses/advanced-gis-interactive-web-mapping-and-spatial-data-visualization
+
+The objective of this website is to allow the user to determine the airports that a given aircraft can take off and land from.
--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A'
 
-var mapCenter = [-95.7129,37.0902]
+var mapCenter = [-96,37]
+
 
 const bounds = [
   [-130,20],
@@ -17,13 +18,100 @@ var map = new mapboxgl.Map({
   maxBounds: bounds
 });
 
-map.on('load', function(){
-  // const marker = new mapboxgl.Marker()
-  //   .setLngLat([Longitude,Latitude])
-  //   .addTo(map);
-
-  map.addSource('airports', {
-    type: 'geojson',
-    data: './data/all-airport-data.geojson'
-  });
+var airport_data= $.getJSON('/data/all-airport-data.json', function(airport_data) {
 });
+
+// airport_data.forEach(function(airportRow){
+//   new mapboxgl.Marker()
+//     .setLngLat([airportRow.Longitude,airportRow.Latitude])
+//     .addTo(map)
+// })
+
+$("#start_airport_btn").click(function(){
+  $('#start_airport').change(function(){
+    var start_airport = $(this).val();
+    console.log(start_airport)
+  })
+  // var start_marker = new mapboxgl.Marker()
+  //   .setLngLat(start_coord)
+  //   .addTo(map);
+});
+
+var createGeoJSONCircle = function(center, radiusInKm, points) {
+  if(!points) points = 64;
+
+  var coords = {
+      latitude: center[1],
+      longitude: center[0]
+  };
+
+  var km = radiusInKm;
+
+  var ret = [];
+  var distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
+  var distanceY = km/110.574;
+
+  var theta, x, y;
+  for(var i=0; i<points; i++) {
+      theta = (i/points)*(2*Math.PI);
+      x = distanceX*Math.cos(theta);
+      y = distanceY*Math.sin(theta);
+
+      ret.push([coords.longitude+x, coords.latitude+y]);
+  }
+  ret.push(ret[0]);
+
+  return {
+      "type": "geojson",
+      "data": {
+          "type": "FeatureCollection",
+          "features": [{
+              "type": "Feature",
+              "geometry": {
+                  "type": "Polygon",
+                  "coordinates": [ret]
+              }
+          }]
+      }
+  };
+};
+
+// map.on('load', function(){
+//   map.loadImage(
+//     './data/airport-icon.png',
+//     (error,image) =>  {
+//       if (error) throw error;
+//       map.addImage('airport-marker', image);
+
+//       map.addSource('airports', {
+//         type: 'geojson',
+//         data: './data/all-airport-data.geojson'
+//       });
+
+//       map.addLayer({
+//         'id': 'points',
+//         'type': 'symbol',
+//         'source': 'airports',
+//         'layout': {
+//           'icon-image': 'airport-marker',
+//           'icon-size': 0.05
+//         }
+//       });
+//     }
+//   );
+  
+//   map.addSource("polygon", createGeoJSONCircle([-93.6248586, 41.58527859], 500));
+
+//   map.addLayer({
+//       "id": "polygon",
+//       "type": "fill",
+//       "source": "polygon",
+//       "layout": {},
+//       "paint": {
+//           "fill-color": "white",
+//           "fill-opacity": 0.6
+//       }
+//   });
+
+
+// });
