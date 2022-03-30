f5eea4d792d56a19c1299390183d8ba1820fc332
MatthewDShen <mds820@nyu.edu>
2022-03-30 16:47:42 -0400

    bunch of stuff

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 The following project is a map generated as the Final Project for URPL-GP 4650. More information on the class can be found here: https://wagner.nyu.edu/education/courses/advanced-gis-interactive-web-mapping-and-spatial-data-visualization
 
-The objective of this website is to allow the user to determine the airports that a given aircraft can take off and land from.
+The objective of this website is to allow the user to select any point around the world and see the range that an aircraft could theoretically fly to
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A'
-
+//Create inital variables
 var mapCenter = [-96,37]
 var aircraft_range = 500
 
@@ -11,6 +10,8 @@ const bounds = [
   [-65,50]
 ];
 
+//Setting up Mabox GL & map//
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A'
 var map = new mapboxgl.Map({
   container: 'mapContainer',
   style: 'mapbox://styles/mapbox/dark-v10',
@@ -19,151 +20,77 @@ var map = new mapboxgl.Map({
   maxBounds: bounds
 });
 
-var createGeoJSONCircle = function(center, radiusInKm, points) {
-  if(!points) points = 64;
-
-  var coords = {
-      latitude: center[1],
-      longitude: center[0]
-  };
-
-  var km = radiusInKm;
-
-  var ret = [];
-  var distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
-  var distanceY = km/110.574;
-
-  var theta, x, y;
-  for(var i=0; i<points; i++) {
-      theta = (i/points)*(2*Math.PI);
-      x = distanceX*Math.cos(theta);
-      y = distanceY*Math.sin(theta);
-
-      ret.push([coords.longitude+x, coords.latitude+y]);
-  }
-  ret.push(ret[0]);
-
-  return {
-      "type": "geojson",
-      "data": {
-          "type": "FeatureCollection",
-          "features": [{
-              "type": "Feature",
-              "geometry": {
-                  "type": "Polygon",
-                  "coordinates": [ret]
-              }
-          }]
-      }
-  };
-};
-
-const coordinates = document.getElementById('coordinates');
-
-const canvas = map.getCanvasContainer();
-
-const geojson = {
-    'type': 'FeatureCollection',
-    'features': [
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': mapCenter
-            }
-        }
-    ]
-};
-
-function onMove(e) {
-    const coords = e.lngLat;
-
-    // Set a UI indicator for dragging.
-    canvas.style.cursor = 'grabbing';
-
-    // Update the Point feature in `geojson` coordinates
-    // and call setData to the source layer `point` on it.
-    geojson.features[0].geometry.coordinates = [coords.lng, coords.lat];
-    map.getSource('point').setData(geojson);
-}
-
-function onUp(e) {
-    const coords = e.lngLat;
-
-    // Print the coordinates of where the point had
-    // finished being dragged to on the map.
-    coordinates.style.display = 'block';
-    coordinates.innerHTML = `Longitude: ${coords.lng}<br />Latitude: ${coords.lat}`;
-    canvas.style.cursor = '';
-
-    // Unbind mouse/touch events
-    map.off('mousemove', onMove);
-    map.off('touchmove', onMove);
-}
-
-map.on('load', function() {
-  // Add a single point to the map.
-  map.addSource('point', {
-      'type': 'geojson',
-      'data': geojson
-  });
-
-  map.addLayer({
-      'id': 'point',
-      'type': 'circle',
-      'source': 'point',
-      'paint': {
-          'circle-radius': 10,
-          'circle-color': '#F84C4C' // red color
-      }
-  });
-
-  // When the cursor enters a feature in
-  // the point layer, prepare for dragging.
-  map.on('mouseenter', 'point', function() {
-      map.setPaintProperty('point', 'circle-color', '#3bb2d0');
-      canvas.style.cursor = 'move';
-  });
-
-  map.on('mouseleave', 'point', function() {
-      map.setPaintProperty('point', 'circle-color', '#3887be');
-      canvas.style.cursor = '';
-  });
-
-  map.on('mousedown', 'point', (e) => {
-      // Prevent the default map drag behavior.
-      e.preventDefault();
-
-      canvas.style.cursor = 'grab';
-
-      map.on('mousemove', onMove);
-      map.once('mouseup', onUp);
-  });
-
-  map.on('touchstart', 'point', (e) => {
-      if (e.points.length !== 1) return;
-
-      // Prevent the default map drag behavior.
-      e.preventDefault();
-
-      map.on('touchmove', onMove);
-      map.once('touchend', onUp);
-  });
-});
-
-$('#aircraft_rng').change(function() {
-  map.addLayer({
-    "id": "polygon",
-    "type": "fill",
-    "source": createGeoJSONCircle(geojson.features[0].geometry.coordinates, aircraft_range),
-    "layout": {},
-    "paint": {
-        "fill-color": "white",
-        "fill-opacity": 0.6
+var airport_data = $.getJSON('/data/all-airport-data.json', function(airport_data){
+    for (var i = 0; i < airport_data.length; i++) {
+        let latlng = [airport_data[i]['Latitude'],airport_data[i]['Longitude']]
+        $('#sel').append('<option value=' + latlng + '>'
+         + airport_data[i]['LocID'] + '</option>');
     }
+    return airport_data
 });
 
 
+$('#sel').change(function(){
+    var coords = $('#sel').val().split(',').map(Number);
+    coords = coords.reverse();
 
 
-    
+
+    const start_marker = new mapboxgl.Marker()
+        .setLngLat(coords)
+        .addTo(map);
+
+    map.addSource("polygon", createGeoJSONCircle(coords, aircraft_range));
+
+    map.addLayer({
+        "id": "polygon",
+        "type": "fill",
+        "source": "polygon",
+        "layout": {},
+        "paint": {
+            "fill-color": "blue",
+            "fill-opacity": 0.6
+        }
+    });
+
+});
+
+var createGeoJSONCircle = function(center, radiusInKm, points) {
+    if(!points) points = 64;
+
+    var coords = {
+        latitude: center[1],
+        longitude: center[0]
+    };
+
+    var km = radiusInKm;
+
+    var ret = [];
+    var distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
+    var distanceY = km/110.574;
+
+    var theta, x, y;
+    for(var i=0; i<points; i++) {
+        theta = (i/points)*(2*Math.PI);
+        x = distanceX*Math.cos(theta);
+        y = distanceY*Math.sin(theta);
+
+        ret.push([coords.longitude+x, coords.latitude+y]);
+    }
+    ret.push(ret[0]);
+
+    return {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [ret]
+                }
+            }]
+        }
+    };
+};
+
